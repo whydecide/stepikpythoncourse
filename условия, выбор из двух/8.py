6c8a43bf13d11d562b5d85cba4d3987f903ec000
whydecide <frolov.yura2000@gmail.com>
2023-12-21 16:29:49 +0700

    Восьмое задание

---
+++ условия, выбор из двух/8.py
@@ -0,0 +1,9 @@
+age = int(input())
+if 1 <= age <= 13:
+    print('детство')
+if 13 < age <= 24:
+    print('молодость')
+if 24 < age <= 59:
+    print('зрелость')
+if age >= 60:
+    print('старость')
